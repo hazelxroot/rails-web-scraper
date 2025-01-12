e74f2afcfd11626dacd0853ee6090fdf6bf0c651
hazelxroot <samh349@gmail.com>
2025-01-12 00:30:22 -0500

    Send emails for successful web scraper results

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,7 +6,13 @@ class Page < ApplicationRecord
   validates :selector, presence: true
   validates :match_text, presence: { if: -> { check_type == "text" } }
 
-  def run_check!
+  def run_and_notify
+    run_check
+    last_result = results.last
+    last_result.notify if last_result.success?
+  end
+
+  def run_check
     scraper = Scraper.new(url)
 
     result = case check_type
